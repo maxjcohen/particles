4d1b2d5966d913949373424a625be28cee4aef23
Max Cohen <max.zagouri@pm.me>
2021-12-14 18:31:33 +0100

    Add torch modified distribution decorator

---
+++ particles/foreign_distributions/__init__.py
@@ -0,0 +1,1 @@
+from .torch import torch_distribution
---
+++ particles/foreign_distributions/torch.py
@@ -0,0 +1,27 @@
+import torch
+
+from ..distributions import ProbDist
+
+
+class TorchModifiedDist(ProbDist):
+    def __init__(self, torch_dist):
+        super().__init__()
+        self.torch_dist = torch_dist
+
+    def shape(self, size):
+        if size is None:
+            return []
+        try:
+            return torch.Size(size)
+        except TypeError:
+            return torch.Size((size,))
+
+    def rvs(self, size=None):
+        return self.torch_dist.sample(self.shape(size)).squeeze()
+
+
+def torch_distribution(torch_dist):
+    def law(*args, **kwargs):
+        return TorchModifiedDist(torch_dist(*args, **kwargs))
+
+    return law
